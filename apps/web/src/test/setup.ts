918cffbea78e6d07b770f503c83f00cb3dca65f6
tsatsu10 <elikemdegbotse@gmail.com>
2026-07-11 21:20:46 +0000

    refactor(web): setup.ts noExplicitAny -> typed

--- apps/web/src/test/setup.ts
+++ apps/web/src/test/setup.ts
@@ -51,7 +51,7 @@ const originalError = console.error;
 const originalWarn = console.warn;
 
 beforeAll(() => {
-  console.error = (...args: any[]) => {
+  console.error = (...args: unknown[]) => {
     if (
       typeof args[0] === "string" &&
       args[0].includes("Warning: ReactDOM.render is no longer supported")
@@ -61,7 +61,7 @@ beforeAll(() => {
     originalError.call(console, ...args);
   };
 
-  console.warn = (...args: any[]) => {
+  console.warn = (...args: unknown[]) => {
     if (
       typeof args[0] === "string" &&
       (args[0].includes("componentWillReceiveProps") ||
